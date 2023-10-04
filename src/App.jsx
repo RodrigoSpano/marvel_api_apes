fb3c4c98b94ae9ca5b1be2534156ac1cfc9abd08
RodrigoSpano <rorrospano17@gmail.com>
2023-10-03 23:19:11 -0300

    navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Route } from "react-router-dom"
 import { Routes } from "react-router-dom"
 import Home from "./views/Home"
 import ComicDetail from "./views/ComicDetail"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
 
   return (
-   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/comic/:id" element={<ComicDetail />} />
-   </Routes>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/comic/:id" element={<ComicDetail />} />
+      </Routes>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from 'react-router-dom'
 import './navbar.scss'
+
 const Navbar = () => {
+  const navigate = useNavigate()
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <img onClick={() => navigate('/')} alt='marvel logo' src='/assets/marvel-logo.png' />
+    </div>
   )
 }
 
