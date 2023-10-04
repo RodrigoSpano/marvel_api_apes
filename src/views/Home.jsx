ae1ef51358b8128410d634c168d1a87170cf1b4f
RodrigoSpano <rorrospano17@gmail.com>
2023-10-04 17:44:29 -0300

    spinner

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,25 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getAllComics } from "../redux/features/comics/comicsActions"
 import ComicsContainer from "../components/comics/ComicsContainer"
 import './home.scss'
+import { Spinner } from "react-bootstrap"
 
 const Home = () => {
 
+  const state = useSelector(state => state.comics)
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(getAllComics())
   }, [])
 
   return (
     <div className="home">
-      <ComicsContainer />
+      {
+        !state.allComics.length ? <Spinner className="spinner" animation="border" /> :
+        <ComicsContainer />
+      }
     </div>
   )
 }
